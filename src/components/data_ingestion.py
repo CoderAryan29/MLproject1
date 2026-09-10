984b9189227684cb97f5070aa33eb71f1a6d1be0
Aryan <aryanmait29@gmail.com>
2026-09-10 23:45:23 +0530

    transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
@@ -25,14 +28,14 @@ class DataIngestion:
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=False)
+            df.to_csv(self.ingestion_config.raw_data_path,index=False)
 
             logging.info("Train Test split Initiated ")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=False)
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False)
 
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=False)
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False)
 
             logging.info('Ingestion of the data is completed')
 
@@ -47,4 +50,7 @@ class DataIngestion:
             raise CustomException(e, sys)
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
